ffeea720555579cd9f67aea4459932fa8d9a230f
root <root@project01.mnks.ch>
2022-11-11 15:42:16 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# スーパー参考書メーカー
+
+## Summary
+
+法政大学情報科学部首藤プロジェクト 2022 秋
+2-10 スーパー参考書メーカーのリポジトリです。
+
+主となる機能は、ISBN とページ数、位置、内容で管理されたデジタル付箋をデータベースで管理、各ユーザが自由に登録、参照することです。
+これらのデータを多く収集することで、参考書における難解な部分を明確にします。その結果、以下のような効果が見込めます。
+
+ - 学習者の躓いたポイントが、他の学習者も同様であるのかを明確にできる
+ - 教師は、多くの学生が躓くポイントを重点的に指導し、そうでない部分に時間を割かないようにできる
+ - 出版社側は、多くの学習者が躓くポイントを把握し、修正できる
+ - 演習の正答率やよくある誤答の蓄積
+
+また、コミュニティを形成することで、以下の結果も得られるでしょう。
+
+ - ユーザ/公式による補足説明
+ - 参考書にはない、演習の別解の掲載
+ - ユーザの学習レベルを設定し統計的に処理することで、各ユーザのレベルに合った参考書をレコメンド
+
+このリポジトリのものは主にバックエンドで稼働する部分です。
+
+HTTP でリクエストを受信し、それに合わせて動作します。
+
+## 検証環境
+
+ - Alma Linux 9
+ - Python 3.9.10
+ - MySQL 8.0.31 (MySQL Community Server - GPL)
+
+## Install
+
+実行に必要な前準備をします。環境によっては不要かもしれません。
+
+```
+dnf install python python3-pip python-devel
+pip3 install -r requirements.txt
+pip3 install mysql-connector-python
+```
+
+MySQL の利用のため、設定を記述します。`app/config.example` を `app/config` にリネームし、適切に書き換えてください。
+
+以上が終わったら、実行します。
+
+```
+cd app
+uvicorn main:app --reload
+```
+
+## Usage
+
+### Registration
+
+以下の curl コマンドのような HTTP リクエストで id と username を指定することで、自動的に uuid が生成され、データベースにユーザが登録されます。
+
+```
+curl -X POST "example.com/register" -H 'Content-Type: application/json' -d '{"id":"foo","username":"Testuser"}'
+```
+
+## DONE
+
+ - ユーザの登録
+
+## TODO
+
+ - 付箋の登録
+   - ISBN, ページ数, 位置, 内容, 付箋の種別
+     - 付箋の種別: 疑問, 補足, 別解等?
+   - 削除
+   - 編集
+ - MySQL 等の設定を外部の config から読み込み
+ - シェルスクリプト or systemd による service 化で実行
+
+### Extra
+
+ - コミュニティ機能の実装
+   - スレッド等
+
+## 参考
+https://qiita.com/yota_dev/items/ab8dea7f71c8a130d5bf
+
+
---
+++ app/main.py
@@ -0,0 +1,111 @@
+#--------#
+# Import #
+#--------#
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mysql.connector
+from mysql.connector import Error
+import uuid
+
+app = FastAPI()
+
+#------------------------#
+# Settings for variables #
+#------------------------#
+
+app = FastAPI()
+db_hostname = "localhost"
+db_username = "root"
+db_password = "jn/aDqLkc6"
+db_name = "superrm"
+db_table_users = "users"
+db_table_books = "books"
+
+#-------------------#---------------------------------------------------------------------#
+# Functions for SQL # https://www.freecodecamp.org/japanese/news/connect-python-with-sql/ #
+#-------------------#---------------------------------------------------------------------#
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL:\tDatabase connection successful")
+    except Error as err:
+        print(f"SQLError:\t'{err}'")
+
+    return connection
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL:\tDatabase connection successful")
+    except Error as err:
+        print(f"SQLError:\t'{err}'")
+
+    return connection
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("MySQL:\tDatabase created successfully")
+    except Error as err:
+        print(f"SQLError:\t'{err}'")
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        value = cursor.fetchall()
+        connection.commit()
+        return value
+    except Error as err:
+        print(f"SQLError:\t'{err}'")
+
+#----------------------#
+# Functions for WebAPI #
+#----------------------#
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+class Register(BaseModel):
+    id: str
+    username: str
+
+@app.post("/register")
+async def register(account: Register):
+    global connection
+    value = execute_query(connection, f"SELECT count(id) FROM {db_table_users} WHERE id='{account.id}'")
+    if value[0][0]==0:
+        uniqueid = str(uuid.uuid4())
+        execute_query(connection, f"INSERT INTO {db_table_users} (uuid, id, username) VALUES ('{uniqueid}', '{account.id}', '{account.username}');")
+        return {"uuid": uniqueid, "id": account.id, "username": account.username}
+    return False
+
+#---------------#
+# Connect to DB #
+#---------------#
+
+connection = create_server_connection(db_hostname, db_username, db_password)
+create_database(connection, "CREATE DATABASE IF NOT EXISTS "+db_name)
+connection = create_db_connection(db_hostname, db_username, db_password, db_name)
+execute_query(connection, f"""CREATE TABLE IF NOT EXISTS {db_table_users}(
+                uuid VARCHAR(36) PRIMARY KEY,
+                id VARCHAR(15),
+                username VARCHAR(50));""")
+execute_query(connection, f"""CREATE TABLE IF NOT EXISTS {db_table_books}(
+                isbn VARCHAR(13) PRIMARY KEY);""")
+
